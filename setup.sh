daf6a38c1e3b7c7e6c6af3b11323a0fba919ece5
Levon Bragg <levon@shiftcs.com>
2025-01-30 15:16:24 -0800

    added setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+cd /usr/phion/home
+wget https://github.com/ShiftComputerServices/bfw-python-tools/archive/refs/tags/Latest.zip
+unzip -jo Latest.zip
+
 rm -f LICENSE
 rm -f scheduler.txt
 rm -f .gitignore
