bd3dbf90510606ec15058cc0fcd66b707369b182
Levon Bragg <levon@shiftcs.com>
2025-01-30 16:22:13 -0800

    modified setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd /usr/phion/home
-wget https://github.com/ShiftComputerServices/bfw-python-tools/archive/refs/tags/Latest.zip
-unzip -jo Latest.zip
+curl https://codeload.github.com/ShiftComputerServices/bfw-python-tools/zip/refs/tags/latest -o latest.zip
+unzip -jo latest.zip
 
 rm -f LICENSE
 rm -f scheduler.txt
 rm -f .gitignore
 rm -f README.md
-rm -f Latest.zip
+rm -f latest.zip
 chmod +x *.py
